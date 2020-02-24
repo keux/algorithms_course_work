ed52f087a8fe0f16247d923c5b5103a9aaff0afd
keux <45452453+keux@users.noreply.github.com>
2020-02-24 17:31:37 +0300

    Create D.cpp

---
+++ bst/D.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+typedef struct _node {
+    int key;
+    struct _node *left, *right, *parent;
+} node;
+
+node* crnode(int val) {
+    node* n = new node;
+    n->key = val;
+    n->left = nullptr;
+    n->right = nullptr;
+    n->parent = nullptr;
+    return n;
+}
+
+node* insert(node *root, int val) {
+    if (nullptr == root)
+        root = crnode(val);
+    if (val < root->key) {
+        if (nullptr == root->left)
+            root->left = crnode(val);
+        else
+            insert(root->left, val);
+    }
+    if (val > root->key) {
+        if (nullptr == root->right)
+            root->right = crnode(val);
+        else
+            insert(root->right, val);
+    }
+    return root;
+}
+
+void print_tree(node *root) {
+    if (root != nullptr) {
+        print_tree(root->left);
+        std::cout << root->key << "\n";
+        print_tree(root->right);
+    }
+}
+
+int height(node *root) {
+    int b = 0, c = 0;
+    if (root != nullptr) {
+        b = height(root->left);
+        c = height(root->right);
+        return std::max(b, c) + 1;
+    } else {
+        return 0;
+    }
+}
+
+int maximum(node *a) {
+    while (a->right != nullptr) {
+        a = a->right;
+    }
+    return a->key;
+}
+
+void leaves(node *root) {
+    if (root != nullptr) {
+        leaves(root->left);
+        if (root->left == nullptr && root->right == nullptr)
+            std::cout << root->key << "\n";
+        leaves(root->right);
+    }
+}
+
+int main() {
+    int a;
+    std::cin >> a;
+    node *root = crnode(a);
+    while (a != 0) {
+        insert(root, a);
+        std::cin >> a;
+    }
+
+    leaves(root);
+    return 0;
+}
