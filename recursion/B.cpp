1c2a2f13fcee15249e8a2518ad350a5ee5724e5b
keux <45452453+keux@users.noreply.github.com>
2020-02-24 17:54:36 +0300

    Create B.cpp

---
+++ recursion/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void move(int n, int x, int y) {
+    int tmp = 6 - x - y;
+    if (n == 1) {
+        std::cout << n << " " << x << " " << tmp << "\n"
+                  << n << " " << tmp << " " << y << "\n";
+    } else {
+        move(n - 1, x, y);
+        std::cout << n << " " << x << " " << tmp << "\n";
+        move(n - 1, y, x);
+        std::cout << n << " " << tmp << " " << y << "\n";
+        move(n - 1, x, y);
+    }
+}
+
+int main() {
+    int numb;
+    std::cin >> numb;
+    move(numb, 1, 3);
+    return 0;
+}
